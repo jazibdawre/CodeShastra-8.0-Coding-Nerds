808f10a8cc7e12664467a066a89046ed3e8896ee
Jazib Dawre <jazib980@gmail.com>
2022-03-27 02:22:10 +0530

    Setup view engine

--- backend/app.js
+++ backend/app.js
@@ -1,71 +1,69 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var passport = require('passport');
-var mongoose = require('mongoose');
-var cors = require('cors')
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var passport = require("passport");
+var mongoose = require("mongoose");
+var cors = require("cors");
 var cron = require("node-cron");
-require('dotenv').config()
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
-const { verifyUser } = require('./authenticate');
-const otpRouter = require('./routes/otp');
-const bankRouter = require('./routes/bank');
-const { scrap } = require('./utils/scrapper');
+const { verifyUser } = require("./authenticate");
+const otpRouter = require("./routes/otp");
+const bankRouter = require("./routes/bank");
+const { scrap } = require("./utils/scrapper");
 
 // Connection To MongoDB
-mongoose.connect(process.env.DB_URL , { useNewUrlParser: true , useUnifiedTopology: true } , (err) => {
-  if(err){
-      console.log(err);
-  }else{
-      console.log("Connected To Database !!");
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Connected To Database !!");
   }
-})
-
+});
 
 // view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
+app.use(cors());
 
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/otp", otpRouter);
+app.use("/banks", bankRouter);
 
-
-app.use(cors())
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/otp',otpRouter);
-app.use('/banks',bankRouter)
-
-// schedule 
+// schedule
 // cron.schedule("0 */2 * * *", () => {
 //   scrap();
 // });
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
+  // render the error page
   res.status(err.status || 500);
-  res.json({err,success:false})
+  res.render("error");
 });
 
 module.exports = app;
